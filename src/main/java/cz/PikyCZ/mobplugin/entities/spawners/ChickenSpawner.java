6e75044b90e6c1cca8e7767c0adc6ea1ce542b12
PikyCZ <jarda.pikart1999@gmail.com>
2017-01-15 20:01:49 +0100

    ..

---
+++ src/main/java/cz/PikyCZ/mobplugin/entities/spawners/ChickenSpawner.java
@@ -0,0 +1,76 @@
+/**
+ * CreeperSpawner.java
+ * 
+ * Created on 10:39:49
+ */
+package cz.PikyCZ.mobplugin.entities.spawners;
+
+import cn.nukkit.IPlayer;
+import cn.nukkit.block.Block;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
+import cn.nukkit.utils.Config;
+import cz.PikyCZ.mobplugin.AutoSpawnTask;
+import cz.PikyCZ.mobplugin.FileLogger;
+import cz.PikyCZ.mobplugin.entities.animal.walking.Chicken;
+import cz.PikyCZ.mobplugin.entities.autospawn.AbstractEntitySpawner;
+import cz.PikyCZ.mobplugin.entities.autospawn.SpawnResult;
+
+/**
+ * Each entity get it's own spawner class.
+ * 
+ * @author <a href="mailto:kniffman@googlemail.com">Michael Gertz</a>
+ */
+public class ChickenSpawner extends AbstractEntitySpawner {
+
+    /**
+     * @param spawnTask
+     */
+    public ChickenSpawner(AutoSpawnTask spawnTask, Config pluginConfig) {
+        super(spawnTask, pluginConfig);
+    }
+
+    public SpawnResult spawn(IPlayer iPlayer, Position pos, Level level) {
+        SpawnResult result = SpawnResult.OK;
+
+        int blockId = level.getBlockIdAt((int) pos.x, (int) pos.y, (int) pos.z);
+        int blockLightLevel = level.getBlockLightAt((int) pos.x, (int) pos.y, (int) pos.z);
+
+        if (blockId != Block.GRASS) { // only spawns on gras
+            result = SpawnResult.WRONG_BLOCK;
+        } else if (pos.y > 127 || pos.y < 1 || level.getBlockIdAt((int) pos.x, (int) pos.y, (int) pos.z) == Block.AIR) { // cannot spawn on AIR block
+            result = SpawnResult.POSITION_MISMATCH;
+        } else { // creeper is spawned
+            this.spawnTask.createEntity(getEntityName(), pos.add(0, 1.7, 0));
+        }
+
+        FileLogger.info(String.format("[%s] spawn for %s at %s,%s,%s with lightlevel %s and blockId %s, result: %s", getLogprefix(), iPlayer.getName(), pos.x, pos.y, pos.z, blockLightLevel, blockId, result));
+
+        return result;
+    }
+    
+    /* (@Override)
+     * @see cn.nukkit.entity.ai.IEntitySpawner#getEntityNetworkId()
+     */
+    @Override
+    public int getEntityNetworkId() {
+        return Chicken.NETWORK_ID;
+    }
+
+    /* (@Override)
+     * @see cn.nukkit.entity.ai.IEntitySpawner#getEntityName()
+     */
+    @Override
+    public String getEntityName() {
+        return "Chicken";
+    }
+    
+    /* (@Override)
+     * @see de.kniffo80.mobplugin.entities.autospawn.AbstractEntitySpawner#getLogprefix()
+     */
+    @Override
+    protected String getLogprefix() {
+        return this.getClass().getSimpleName();
+    }
+
+}
