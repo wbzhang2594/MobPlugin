1ea692ce3cc111d3d11f1a5d396c482f08ac7acc
PikyCZ <jarda.pikart1999@gmail.com>
2017-01-15 12:48:13 +0100

    Add files via upload

---
+++ src/main/java/PikyCZ/mobplugin/entities/autospawn/IEntitySpawner.java
@@ -0,0 +1,28 @@
+/**
+ * EntitySpawner.java
+ * 
+ * Created on 10:38:53
+ */
+package PikyCZ.mobplugin.entities.autospawn;
+
+import java.util.Collection;
+
+import cn.nukkit.IPlayer;
+import cn.nukkit.Player;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
+
+/**
+ * @author <a href="mailto:kniffman@googlemail.com">Michael Gertz</a>
+ */
+public interface IEntitySpawner {
+
+    public void spawn(Collection<Player> onlinePlayers);
+
+    public SpawnResult spawn(IPlayer iPlayer, Position pos, Level level);
+    
+    public int getEntityNetworkId ();
+    
+    public String getEntityName ();
+
+}
