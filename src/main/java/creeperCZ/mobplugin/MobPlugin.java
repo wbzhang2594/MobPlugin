1b11116ce98b72884b58fc0aa90a0b570f957845
caspervanneck <wesleyvanneck1012@gmail.com>
2017-01-18 19:46:50 +0100

    Update MobPlugin.java

--- src/main/java/creeperCZ/mobplugin/MobPlugin.java
+++ src/main/java/creeperCZ/mobplugin/MobPlugin.java
@@ -86,10 +86,8 @@ public class MobPlugin extends PluginBase implements Listener {
     public void onEnable() {
         // Config reading and writing
         saveDefaultConfig();
-        pluginConfig = getConfig();
-	this.getDataFolder().mkdirs();	
-        //this.loadAll();
-       
+	reloadConfig();
+
         // we need this flag as it's controlled by the plugin's entities
         MOB_AI_ENABLED = pluginConfig.getBoolean("entities.mob-ai", true);
         int spawnDelay = pluginConfig.getInt("entities.auto-spawn-tick", 2500);
