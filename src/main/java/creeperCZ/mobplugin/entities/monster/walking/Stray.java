fde65bfcdd0c0ad373c85237a04efea6de06c729
PikyCZ <jarda.pikart1999@gmail.com>
2017-03-31 21:54:49 +0200

    update Stray base tick

--- src/main/java/creeperCZ/mobplugin/entities/monster/walking/Stray.java
+++ src/main/java/creeperCZ/mobplugin/entities/monster/walking/Stray.java
@@ -117,7 +117,7 @@ public class Stray extends WalkingMonster {
         hasUpdate = super.entityBaseTick(tickDiff);
 
         int time = this.getLevel().getTime() % Level.TIME_FULL;
-        if (!this.isOnFire() && !this.level.isRaining() && (time < Level.TIME_NIGHT || time > Level.TIME_SUNRISE)) {
+        if (!this.isOnFire() && !this.level.isRaining() && (time > Level.TIME_SUNSET && time >= 140000 && time < Level.TIME_SUNRISE)) {
             this.setOnFire(100);
         }
 
