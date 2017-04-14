2b59a9c425b00d341dd32c3d2979ffc495d1830c
CreeperFace <honzaxp01@gmail.com>
2017-04-14 19:43:13 +0200

    Add creeper fuse

---
+++ src/main/java/com/pikycz/mobplugin/entities/animal/walking/Sheep.java
@@ -0,0 +1,183 @@
+package com.pikycz.mobplugin.entities.animal.walking;
+
+import cn.nukkit.Player;
+import cn.nukkit.block.BlockAir;
+import cn.nukkit.block.BlockWool;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.entity.EntityHumanType;
+import cn.nukkit.entity.data.ByteEntityData;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemDye;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.DyeColor;
+import com.pikycz.mobplugin.entities.animal.WalkingAnimal;
+import com.pikycz.mobplugin.entities.utils.Utils;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Sheep extends WalkingAnimal {
+
+    public static final int NETWORK_ID = 13;
+
+    public boolean sheared = false;
+    public int color = 0;
+
+    public Sheep(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+
+    @Override
+    public float getWidth() {
+        return 0.9f;
+    }
+
+    @Override
+    public float getHeight() {
+        if (isBaby()) {
+            return 0.9f; // No have information
+        }
+        return 1.3f;
+    }
+
+    @Override
+    public float getEyeHeight() {
+        if (isBaby()) {
+            return 0.95f * 0.9f; // No have information
+        }
+        return 0.95f * getHeight();
+    }
+
+    @Override
+    public void initEntity() {
+        this.setMaxHealth(8);
+        if (!this.namedTag.contains("Color")) {
+            this.setColor(this.randomColor());
+        } else {
+            this.setColor(this.namedTag.getByte("Color"));
+        }
+
+        if (!this.namedTag.contains("Sheared")) {
+            this.namedTag.putByte("Sheared", 0);
+        } else {
+            this.sheared = this.namedTag.getBoolean("Sheared");
+        }
+
+        this.setDataFlag(0, 26, this.sheared);
+    }
+
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        this.namedTag.putByte("Color", this.color);
+        this.namedTag.putBoolean("Sheared", this.sheared);
+    }
+
+    @Override
+    public boolean targetOption(EntityCreature creature, double distance) {
+        if (creature instanceof Player) {
+            Player player = (Player) creature;
+            return player.spawned && player.isAlive() && !player.closed && player.getInventory().getItemInHand().getId() == Item.SEEDS && distance <= 49;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInteract(Entity entity, Item item) {
+        if (item.getId() == Item.DYE) {
+            this.setColor(((ItemDye) item).getDyeColor().getWoolData());
+
+            if (entity instanceof EntityHumanType) { //TODO: change this in nukkit
+                EntityHumanType human = (EntityHumanType) entity;
+                item.setCount(item.getCount() - 1);
+
+                if (item.getCount() <= 0) {
+                    human.getInventory().setItemInHand(new ItemBlock(new BlockAir()));
+                } else {
+                    human.getInventory().setItemInHand(item);
+                }
+            }
+
+            return true;
+        }
+
+        if (item.getId() == Item.WHEAT) {
+
+        }
+
+        if (item.getId() != Item.SHEARS) {
+            return super.onInteract(entity, item);
+        }
+
+        if (shear()) {
+            if (entity instanceof EntityHumanType) {
+                EntityHumanType human = (EntityHumanType) entity;
+                item.setDamage(item.getDamage() + 1);
+
+                if (item.getDamage() >= item.getMaxDurability()) {
+                    human.getInventory().setItemInHand(new ItemBlock(new BlockAir()));
+                } else {
+                    human.getInventory().setItemInHand(item);
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean shear() {
+        if (sheared) {
+            return false;
+        }
+
+        this.sheared = true;
+        this.setDataFlag(DATA_FLAGS, DATA_FLAG_SHEARED, true);
+
+        this.level.dropItem(this, new ItemBlock(new BlockWool(this.getColor()), 0, this.level.rand.nextInt(2) + 1));
+        return true;
+    }
+
+    @Override
+    public Item[] getDrops() {
+        if (this.lastDamageCause instanceof EntityDamageByEntityEvent) {
+            return new Item[]{Item.get(Item.WOOL, getColor(), 1)};
+        }
+        return new Item[0];
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+        this.setDataProperty(new ByteEntityData(DATA_COLOUR, color));
+    }
+
+    public int getColor() {
+        return namedTag.getByte("Color");
+    }
+
+    private int randomColor() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        double rand = random.nextDouble(1, 100);
+
+        if (rand <= 15) {
+            return random.nextBoolean() ? DyeColor.BLACK.getWoolData() : random.nextBoolean() ? DyeColor.GRAY.getWoolData() : DyeColor.LIGHT_GRAY.getWoolData();
+        }
+
+        return DyeColor.WHITE.getWoolData();
+    }
+
+    @Override
+    public int getKillExperience() {
+        return Utils.rand(1, 4); // gain 1-3 experience
+    }
+
+}
---
+++ src/main/java/com/pikycz/mobplugin/entities/autospawn/AbstractEntitySpawner.java
@@ -0,0 +1,143 @@
+package com.pikycz.mobplugin.entities.autospawn;
+
+import cn.nukkit.IPlayer;
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
+import cn.nukkit.utils.Config;
+import com.pikycz.mobplugin.AutoSpawnTask;
+import com.pikycz.mobplugin.FileLogger;
+import com.pikycz.mobplugin.entities.utils.Utils;
+
+import java.util.*;
+
+/**
+ * @author <a href="mailto:kniffman@googlemail.com">Michael Gertz</a>
+ */
+public abstract class AbstractEntitySpawner implements IEntitySpawner {
+
+    protected AutoSpawnTask spawnTask;
+
+    protected Server server;
+
+    protected List<String> disabledSpawnWorlds = new ArrayList<>();
+
+    protected static Random random = new Random();
+
+    public AbstractEntitySpawner(AutoSpawnTask spawnTask, Config pluginConfig) {
+        this.spawnTask = spawnTask;
+        this.server = Server.getInstance();
+        String disabledWorlds = pluginConfig.getString("entities.worlds-spawn-disabled");
+        if (disabledWorlds != null && !disabledWorlds.trim().isEmpty()) {
+            StringTokenizer tokenizer = new StringTokenizer(disabledWorlds, ",");
+            while (tokenizer.hasMoreTokens()) {
+                disabledSpawnWorlds.add(tokenizer.nextToken());
+            }
+            FileLogger.debug(String.format("[%s] Disabled spawn for the following worlds: %s", getLogprefix(), disabledSpawnWorlds));
+        }
+    }
+
+    /*
+     * (@Override)
+     * @see cn.nukkit.entity.ai.IEntitySpawner#spawn(java.util.List, java.util.List)
+     */
+    public void spawn(Collection<Player> onlinePlayers) {
+        // first spawn everything for online players ...
+        if (isSpawnAllowedByDifficulty()) {
+            SpawnResult lastSpawnResult = null;
+            for (Player player : onlinePlayers) {
+                if (isWorldSpawnAllowed(player.getLevel())) {
+                    lastSpawnResult = spawn(player);
+                    if (lastSpawnResult.equals(SpawnResult.MAX_SPAWN_REACHED)) {
+                        break;
+                    }
+                }
+            }
+        } else {
+            FileLogger.debug(String.format("[%s] Spawn not allowed because of difficulty [entityName:%s]", getLogprefix(), getEntityName()));
+        }
+
+    }
+
+    /**
+     * Checks if the given level's name is on blacklist for auto spawn
+     *
+     * @param level the level to be checked
+     * @return <code>true</code> when world spawn is allowed
+     */
+    private boolean isWorldSpawnAllowed(Level level) {
+        for (String worldName : this.disabledSpawnWorlds) {
+            if (level.getName().toLowerCase().equals(worldName.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected SpawnResult spawn(IPlayer iPlayer) {
+        // boolean offlinePlayer = iPlayer instanceof OfflinePlayer;
+        //
+        // Level level = offlinePlayer ? ((OfflinePlayer) iPlayer).getLevel() : ((Player) iPlayer).getLevel();
+        //
+        // if (!isEntitySpawnAllowed(level)) {
+        // return SpawnResult.MAX_SPAWN_REACHED;
+        // }
+        //
+        // Position pos = offlinePlayer ? ((OfflinePlayer) iPlayer).getLastKnownPosition() : ((Player) iPlayer).getPosition();
+        Position pos = ((Player) iPlayer).getPosition();
+        Level level = ((Player) iPlayer).getLevel();
+
+        if (this.spawnTask.entitySpawnAllowed(level, getEntityNetworkId(), getEntityName())) {
+            if (pos != null) {
+                // get a random safe position for spawn
+                pos.x += this.spawnTask.getRandomSafeXZCoord(50, 26, 6);
+                pos.z += this.spawnTask.getRandomSafeXZCoord(50, 26, 6);
+                pos.y = this.spawnTask.getSafeYCoord(level, pos, 3);
+            }
+
+            if (pos == null) {
+                return SpawnResult.POSITION_MISMATCH;
+            }
+        } else {
+            return SpawnResult.MAX_SPAWN_REACHED;
+        }
+
+        return spawn(iPlayer, pos, level);
+    }
+
+    /**
+     * A simple method that evaluates based on the difficulty set in server if a spawn is allowed or not
+     *
+     * @return
+     */
+    protected boolean isSpawnAllowedByDifficulty() {
+
+        int randomNumber = Utils.rand(0, 4);
+
+        switch (getCurrentDifficulty()) {
+            case PEACEFUL:
+                return randomNumber == 0;
+            case EASY:
+                return randomNumber <= 1;
+            case NORMAL:
+                return randomNumber <= 2;
+            case HARD:
+                return true; // in hard: always spawn
+            default:
+                return true;
+        }
+    }
+
+    /**
+     * Returns currently set difficulty as en {@link Enum}
+     *
+     * @return a {@link Difficulty} instance
+     */
+    protected Difficulty getCurrentDifficulty() {
+        return Difficulty.getByDiffculty(this.server.getDifficulty());
+    }
+
+    protected abstract String getLogprefix();
+
+}
